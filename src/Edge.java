7fddf8bd3adbc085457aab643939541de08e27e0
ShayanthS <ssinnarajah@berkeley.edu>
2013-05-01 02:23:39 -0700

    Finished Kruskal, changed Edge

--- src/Edge.java
+++ src/Edge.java
@@ -1,12 +1,53 @@
 
-
+// This will be a similar representation to VPair, but with the use of edges instead of the VertexPari
 public class Edge {
-	public Object[] vertices;
-	public int weight;
+	protected Object object1;
+	protected Object object2;
+	protected int weight;
 	
-	public Edge(Object[] vert, int wt){
-		vertices = vert;
-		weight= wt;
+	public Edge(Object obj1, Object obj2, int wt){
+		obj1 = object1;
+		obj2 = object2;
+		wt = weight;
 	}
+	/**
+	   * hashCode() returns a hashCode equal to the sum of the hashCodes of each
+	   * of the two objects of the pair, so that the order of the objects will
+	   * not affect the hashCode.  Self-edges are treated differently:  we don't
+	   * add an object's hashCode to itself, since the result would always be even.
+	   * We add one to the hashCode so that a self-edge will not collide with the
+	   * object itself if vertices and edges are stored in the same hash table.
+	   */
+	  public int hashCode() {
+	    if (object1.equals(object2)) {
+	      return object1.hashCode() + 1;
+	    } else {
+	      return object1.hashCode() + object2.hashCode();
+	    }
+	  }
 
+	  /**
+	   * equals() returns true if this VertexPair represents the same unordered
+	   * pair of objects as the parameter "o".  The order of the pair does not
+	   * affect the equality test, so (u, v) is found to be equal to (v, u).
+	   */
+	  public boolean equals(Object o) {
+	    if (o instanceof Edge) {
+	      return ((object1.equals(((Edge) o).object1)) &&
+	              (object2.equals(((Edge) o).object2))) ||
+	             ((object1.equals(((Edge) o).object2)) &&
+	              (object2.equals(((Edge) o).object1)));
+	    } else {
+	      return false;
+	    }
+	  }
+	  public Object v1(){
+		  return object1;
+	  }
+	  public Object v2(){
+		  return object2;
+	  }
+	  public int weight(){
+		  return weight;
+	  }
 }
--- src/Kruskal.java
+++ src/Kruskal.java
@@ -1,5 +1,6 @@
 /* Kruskal.java */
 
+import dict.HashTableChained;
 import graph.*;
 import set.*;
 
@@ -10,11 +11,126 @@ import set.*;
 
 public class Kruskal {
 
-  /**
-   * minSpanTree() returns a WUGraph that represents the minimum spanning tree
-   * of the WUGraph g.  The original WUGraph g is NOT changed.
-   */
-/*  public static WUGraph minSpanTree(WUGraph g);
+	/**
+	 * minSpanTree() returns a WUGraph that represents the minimum spanning tree
+	 * of the WUGraph g.  The original WUGraph g is NOT changed.
+	 */
+	public static WUGraph minSpanTree(WUGraph g){ 
+		WUGraph t = new WUGraph(); 
+		int indexedge = 0;
+		int counter = 0;
+		Object[] gVert = g.getVertices();
+		Object[] edgearray = new Object[2];
+		Edge[] edgesobjects = new Edge[2 * g.edgeCount() + 1];
+		for (int i = 0; i < gVert.length; i++) {
+			Neighbors neighbors = g.getNeighbors(gVert[i]);
+				for (int j = 0; j < neighbors.neighborList.length; j++) {
+						edgesobjects[indexedge] = new Edge(gVert[i], neighbors.neighborList[j], neighbors.weightList[j]);
+						indexedge += 1;
+				}
+			}
+		edgearray[0] = edgesobjects;
+		edgearray[1] = indexedge - 1;
+		Object[] IndexEdges = edgearray;
+		Edge[] edges = (Edge []) IndexEdges[0];
+		HashTableChained gTable = new HashTableChained(gVert.length);
+		DisjointSets sets = new DisjointSets(gTable.size());
+		for (int i = 0; i < gVert.length; i++){
+			g.addVertex(gVert[i]);
+			gTable.insert(gVert[i], counter);
+			counter += 1;
+		}
+		quicksort(edges, 0, (Integer) IndexEdges[1]);
+		for (int i = 0; i < (Integer) IndexEdges[1] + 1; i++){ // Need to add one in order to access every element
+			int v1 = sets.find((Integer) (gTable.find(edges[i].v1()).value()));
+			int v2 = sets.find((Integer) (gTable.find(edges[i].v2()).value()));
+			if (v1 != v2){
+				sets.union(v1, v2);
+				t.addEdge(edges[i].v1(), edges[i].v2(), edges[i].weight());
+			}
+		}
+		return t;
+		
+	}
 
-}*/
+	
+	/**
+	 * Method to swap two ints in an array.
+	 * 
+	 * @param a
+	 *            an array of ints.
+	 * @param index1
+	 *            the index of the first int to be swapped.
+	 * @param index2
+	 *            the index of the second int to be swapped.
+	 **/
+	public static void swapReferences(Edge[] a, int index1, int index2) {
+		Edge tmp = a[index1];
+		a[index1] = a[index2];
+		a[index2] = tmp;
+	}
+
+	/**
+	 * This is a generic version of C.A.R Hoare's Quick Sort algorithm. This
+	 * will handle arrays that are already sorted, and arrays with duplicate
+	 * keys.
+	 * 
+	 * If you think of an array as going from the lowest index on the left to
+	 * the highest index on the right then the parameters to this function are
+	 * lowest index or left and highest index or right. The first time you call
+	 * this function it will be with the parameters 0, a.length - 1.
+	 * 
+	 * @param a
+	 *            an integer array
+	 * @param lo0
+	 *            left boundary of array partition
+	 * @param hi0
+	 *            right boundary of array partition
+	 **/
+	private static void quicksort(Edge a[], int lo0, int hi0) {
+		int lo = lo0;
+		int hi = hi0;
+		int mid;
+
+		if (hi0 > lo0) {
+
+			// Arbitrarily establishing partition element as the midpoint of
+			// the array.
+			swapReferences(a, lo0, (lo0 + hi0) / 2);
+			mid = a[(lo0 + hi0) / 2].weight;
+
+			// loop through the array until indices cross.
+			while (lo <= hi) {
+				// find the first element that is greater than or equal to
+				// the partition element starting from the left Index.
+				while ((lo < hi0) && (a[lo].weight < mid)) {
+					lo++;
+				}
+
+				// find an element that is smaller than or equal to
+				// the partition element starting from the right Index.
+				while ((hi > lo0) && (a[hi].weight > mid)) {
+					hi--;
+				}
+				// if the indices have not crossed, swap them.
+				if (lo <= hi) {
+					swapReferences(a, lo, hi);
+					lo++;
+					hi--;
+				}
+			}
+
+			// If the right index has not reached the left side of array
+			// we must now sort the left partition.
+			if (lo0 < hi) {
+				quicksort(a, lo0, hi);
+			}
+
+			// If the left index has not reached the right side of array
+			// must now sort the right partition.
+			if (lo < hi0) {
+				quicksort(a, lo, hi0);
+			}
+		}
+	}
 }
